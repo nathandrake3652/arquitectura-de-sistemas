e9b91c63d521bb5a4a2843378bb1b2ed70a424d8
Matias364 <matias.cortes09@alumnos.ucn.cl>
2026-04-21 13:27:11 -0400

    feat(page home and inventory)
    Se agregó la librería de jinja2 y python-multipart para su uso en las nuevas paginas. Se está usando en general jinja2 + HTMX y TailwindCSS. Se agregó parametro --reload en docker compose para que los cambios surtan efecto en la imagen docker

--- app/main.py
+++ app/main.py
@@ -1,13 +1,19 @@
 from contextlib import asynccontextmanager
 
-from fastapi import FastAPI
+from fastapi import Depends, FastAPI, Form
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+from sqlalchemy.orm import Session
 from app.core.exceptions import DomainException
 from fastapi import Request
 from fastapi.responses import JSONResponse
 
 from app.api.v1.router import api_router
 from app.core.config import settings
+from app.crud.ingredient import get_ingredient, get_ingredients
 from app.db.base import init_db
+from app.db.session import get_db
+from app.services.inventory_service import InventoryService
 
 
 @asynccontextmanager
@@ -17,6 +23,9 @@ async def lifespan(_: FastAPI):
 
 
 app = FastAPI(title=settings.app_name, lifespan=lifespan)
+app.mount("/static", StaticFiles(directory="app/static"), name="static")
+templates = Jinja2Templates(directory="app/templates")
+
 app.include_router(api_router, prefix="/api/v1")
 
 @app.exception_handler(DomainException)
@@ -35,3 +44,31 @@ async def domain_exception_handler(request: Request, exc: DomainException):
 @app.get("/health", tags=["health"])
 def health_check():
     return {"status": "ok", "environment": settings.app_env}
+
+@app.get("/", tags=["UI"])
+async def read_root(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
+@app.get("/ui/inventory", tags=["UI"])
+async def render_inventory(request: Request, db: Session = Depends(get_db)):
+    ingredients = get_ingredients(db)
+    return templates.TemplateResponse("inventory.html", {"request": request, "ingredients": ingredients})
+
+@app.post("/ui/inventory/{ingredient_id}/adjust", tags=["UI"])
+async def adjust_inventory(
+    request: Request,
+    ingredient_id: int,
+    amount: float = Form(...),
+    reason: str = Form(...),
+    db: Session = Depends(get_db)
+):
+    inventory_service = InventoryService(db)
+    inventory_service.adjust_stock(
+        ingredient_id=ingredient_id,
+        amount=amount,
+        reason=reason
+    )
+
+    update_ingredient = get_ingredient(db, ingredient_id)
+
+    return templates.TemplateResponse("inventory_row.html", {"request": request, "ingredient": update_ingredient})
